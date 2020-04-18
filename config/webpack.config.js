141f24c6cd4585002d213129d9e7882b56e2ff0f
Nurbek Mambetaliev <Nurbek_Mambetaliev@epam.com>
2020-04-18 12:09:54 +0300

    add cross-env

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,32 +1,37 @@
 const webpack = require('webpack');
 const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
-console.log(path.resolve(__dirname, './build'));
-module.exports = {
-  entry: [path.resolve(__dirname, '../client/index.js')],
-  output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: "main.js"
-  },
-  plugins: [new HtmlWebPackPlugin({template: path.resolve(__dirname, './index.html')})],
-  devServer: {
-    hot: true,
-    watchContentBase: true,
-    port: 3000
-  },
-  devtool: 'source-map',
-  module: {
-    rules: [
-      {
-        test: /.js?$/,
-        exclude: /node_modules/,
-        loader: "babel-loader",
-      },
-      {
-        enforce: "pre",
-        test: /\.js$/,
-        loader: "source-map-loader"
-      }
-    ]
+
+module.exports = function (webpackEnv) {
+  const isEnvDevelopment = process.env.NODE_ENV === 'development';
+  const isEnvProduction = process.env.NODE_ENV === 'production';
+
+  return {
+    entry: [path.resolve(__dirname, '../client/index.js')],
+    output: {
+      path: path.resolve(__dirname, 'build'),
+      filename: "main.js"
+    },
+    plugins: [new HtmlWebPackPlugin({template: path.resolve(__dirname, './index.html')})],
+    devServer: isEnvDevelopment ? {
+      hot: true,
+      watchContentBase: true,
+      port: 3000
+    } : undefined,
+    devtool: isEnvDevelopment ? 'source-map' : undefined,
+    module: {
+      rules: [
+        {
+          test: /.js?$/,
+          exclude: /node_modules/,
+          loader: "babel-loader",
+        },
+        {
+          enforce: "pre",
+          test: /\.js$/,
+          loader: "source-map-loader"
+        }
+      ]
+    }
   }
 }
--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'config', 'build')));
 app.get('/*', (req, res) => {
-    console.log(path.join(__dirname, 'config', 'build', 'index.html'));
     res.sendFile(path.join(__dirname, 'config', 'build', 'index.html'));
 });
 app.listen(3000);
